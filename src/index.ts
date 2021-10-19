5472695b0698b84dd62991b637595d3124147af7
Jamyth Luk <jamythluk@jamyth.local>
2021-10-19 11:16:55 +0800

    [INIT]: type-game: initialize project using Core-Codegen

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { startApp, async } from 'coil-react';
+
+const MainComponent = async(() => import('module/main'), 'MainComponent');
+
+startApp({
+    MainComponent,
+    entryElement: document.getElementById('app'),
+});
